8cd2b87468a65857ac04a9eed54967544e447e7f
lynn7mitchell <lynn7mitchell@gmail.com>
2020-06-15 11:54:22 -0500

    added cat exercise to classes.py

--- classes.py
+++ classes.py
@@ -1,13 +1,21 @@
-#OOP
-#Classes
+# OOP
+# Classes
 
 class PlayerCharacter:
+    # Class Object Attribute
+    membership = True
+
     # __init__ is a magic method or a constructor
     # this means you have to give the argument for name
-    #'self' is similar to 'this' in js 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    # 'self' is similar to 'this' in js
+    def __init__(self, name='anonymous', age=0):
+        if(PlayerCharacter.membership):
+            self.name = name
+            self.age = age
+
+    def shout(self):
+        print(f'my name is {self.name}')
+        return 'done shouting'
 
     def run(self):
         print('run')
@@ -26,3 +34,38 @@ print(player2.name, player2.age)
 
 
 print(player2.attack)
+
+print(player1.shout())
+
+
+
+#Given the below class:
+class Cat:
+    species = 'mammal'
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+# 1 Instantiate the Cat object with 3 cats
+kaliki = Cat('Kaliki', 3)
+toby = Cat('Toby', 5)
+kraz = Cat('Kraz', 4)
+
+
+# 2 Create a function that finds the oldest cat
+def oldestCat(*cats):
+    highestAge = 0
+    for cat in cats:
+        if(cat.age > highestAge):
+            highestAge = cat.age
+    return highestAge
+        
+# Solution
+# def get_oldest_cat(*args):
+#     return max(args)
+    
+oldestCat(kaliki, toby, kraz)
+
+# 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
+print(f"The oldest cat is {oldestCat(kaliki, toby, kraz)} years old.")
