3729ba0f4f5e3e96bf7af8fd093831aad344d0d6
lynn7mitchell <lynn7mitchell@gmail.com>
2020-07-08 20:14:35 -0500

    added a html template to email_playground

--- Scripting/email_playground/email_sender.py
+++ Scripting/email_playground/email_sender.py
@@ -1,20 +1,24 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path #similar to os.path
 
-
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Lynn Mitchell'
 email['to'] = 'code7sandbox@gmail.com'
 email['subject'] = 'Python Email'
 
 
-email.set_content('This email was sent with Python!')
+# email.set_content('This email was sent with Python!')
+email.set_content(html.substitute({'name': 'Name Namerson'} ), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     # start server
     smtp.ehlo()
     # ttls is encryption
     smtp.starttls()
+    # dummy email
     smtp.login('code7sandbox@gmail.com', 'CodeEntry!==false')
     smtp.send_message(email)
     print('it worked')
