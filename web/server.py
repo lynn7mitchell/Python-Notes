b8f57935d5cedefc9b5e596bd11cc26ccc79d789
lynn7mitchell <lynn7mitchell@gmail.com>
2020-08-07 20:01:40 -0500

    added use of URL Paramaters in flask

--- web/server.py
+++ web/server.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+# have to have /username
+@app.route('/<username>')
+def home(username = None):
+    return render_template('index.html', name=username)
+
+@app.route('/<username>/<int:post_id>')
+def home_with_id(username = None, post_id=None):
+    return render_template('index.html', name=username, post_id=post_id)
