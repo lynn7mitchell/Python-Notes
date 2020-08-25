9aded084378ffcd6234d612713cf41acdac0aaf7
lynn7mitchell <lynn7mitchell@gmail.com>
2020-08-24 21:05:10 -0500

    added ml_steps and a jupyter notebook

---
+++ Machine Learning/ml_steps.py
@@ -0,0 +1,9 @@
+#1 Import the data
+#2 Clean the data
+#3 Split the data. Training Set / Test Set
+#4 Create a Model (Import an algorithm or library)
+#5 Check the output (with test set)
+#6 Improve (extra input)
+
+
+#NumPy Pandas scikit matplotlib kaggle
