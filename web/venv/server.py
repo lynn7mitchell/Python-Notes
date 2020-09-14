448d93c076411409032e7e449a3f0f42403cd954
Lynn Mitchell <lynn7mitchell@gmail.com>
2020-09-13 22:30:00 -0500

    form can post data (no database)

--- web/venv/server.py
+++ web/venv/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 print(__name__)
 
@@ -20,27 +20,44 @@ def test():
 
 
 @app.route('/')
-@app.route('/index.html')
+# @app.route('/index.html')
 def home():
     return render_template('./index.html')
 
-@app.route('/about.html')
-def about():
-    return render_template('about.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('/thankyou.html')
+    else:
+        error = 'something went wrong'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return 'form submitted'
+
+# @app.route('/about.html')
+# def about():
+#     return render_template('about.html')
     
 
-@app.route('/components.html')
-def components():
-    return render_template('/components.html')
+# @app.route('/components.html')
+# def components():
+#     return render_template('/components.html')
 
-@app.route('/contact.html')
-def contact():
-    return render_template('/contact.html')
+# @app.route('/contact.html')
+# def contact():
+#     return render_template('/contact.html')
 
-@app.route('/work.html')
-def work():
-    return render_template('/work.html')
+# @app.route('/work.html')
+# def work():
+#     return render_template('/work.html')
 
-@app.route('/works.html')
-def works():
-    return render_template('/works.html')
+# @app.route('/works.html')
+# def works():
+#     return render_template('/works.html')
