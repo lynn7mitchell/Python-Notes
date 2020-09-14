6d4b1b7bb92eb303666710107824d2c2d9d298fe
Lynn Mitchell <lynn7mitchell@gmail.com>
2020-09-13 20:24:40 -0500

    added mashup template

--- web/venv/server.py
+++ web/venv/server.py
@@ -2,10 +2,45 @@ from flask import Flask, render_template
 app = Flask(__name__)
 print(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+# variable rules
+# https://flask.palletsprojects.com/en/1.1.x/quickstart/#variable-rules
+
+# <> is a dynamic parameter example: www.website.com/lynn would mean <username> = lynn then set default = None
+# int means it HAS TO be an integer
+# @app.route('/<username>/<int:post_id>')
+# pass username as a prarameter and set a default
+# def hello_world(username=None, post_id=None):
+# name = name(lynn)
+# now name is a variable that can be used in your templates
+    # return render_template('index.html', name=username, post_id=post_id)
 
 @app.route('/test')
 def test():
     return 'test!'
+
+
+@app.route('/')
+@app.route('/index.html')
+def home():
+    return render_template('./index.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+    
+
+@app.route('/components.html')
+def components():
+    return render_template('/components.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('/contact.html')
+
+@app.route('/work.html')
+def work():
+    return render_template('/work.html')
+
+@app.route('/works.html')
+def works():
+    return render_template('/works.html')
